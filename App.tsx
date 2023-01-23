c3a6e118021b976f3042f02ff055f14a37fb421e
kagiso <kagisojamesthomba@gmail.com>
2023-01-23 08:43:12 +0200

    installed expo

--- App.tsx
+++ App.tsx
@@ -3,11 +3,12 @@ import {StyleSheet, View} from 'react-native';
 import CommentsScreen from './src/screens/CommentsScreen';
 import ProfileScreen from './src/screens/ProfileScreen/index';
 import EditProfileScreen from './src/screens/EditProfileScreen/index';
+import PostUploadScreen from './src/screens/PostUploadScreen/index';
 
 const App = () => {
   return (
     <View style={styles.app}>
-      <EditProfileScreen />
+      <PostUploadScreen />
     </View>
   );
 };
